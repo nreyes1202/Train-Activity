39b32775f516041181fd902e87d07438c82df92f
Nilsen Reyes <nreyes1202@yahoo.com>
2020-02-10 22:54:53 -0700

    created API key and started firebase function in js file

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+//web API key AIzaSyBCbmehTNwvQ_hTTvi2-L77R5RznS_PQ_E 
+
+// 1. Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBCbmehTNwvQ_hTTvi2-L77R5RznS_PQ_E ",
+    authDomain: "time-sheet-55009.firebaseapp.com",
+    databaseURL: "https://time-sheet-55009.firebaseio.com",
+    storageBucket: "time-sheet-55009.appspot.com"
+  };
+  
+  firebase.initializeApp(config);
+  
+  var database = firebase.database();
